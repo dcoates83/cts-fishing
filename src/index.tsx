0aac8372964893f7d1f0fac16eb74d81f3672f3b
Devon <devon.coates@totaleto.com>
2022-04-29 17:00:40 -0600

    build(add files): added settup files BREAKING CHANGE:

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import CssBaseline from "@mui/material/CssBaseline";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
@@ -8,6 +9,7 @@ const root = createRoot(container);
 
 root.render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>
 );
